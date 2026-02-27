fedbc597f80102a1c83fdf3f4cecdbc11ac54ffc
Marvin Gudiel <mgudiell@miumg.edu.gt>
2026-02-27 14:36:47 -0500

    ejercicio busqueda binaria y lineal

---
+++ Metodos de busqueda/Program.cs
@@ -0,0 +1,85 @@
+﻿using System;
+
+namespace Busquedas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numeros = { 2, 5, 8, 12, 16, 23, 38, 45, 56, 72 };
+
+            Console.WriteLine("Numeros en el arreglo:");
+            for (int i = 0; i < numeros.Length; i++)
+            {
+                Console.Write(numeros[i] + " ");
+            }
+
+            Console.WriteLine("\n\nIngrese el numero que desea buscar:");
+            int numeroBuscar = int.Parse(Console.ReadLine());
+
+            int resultadoLineal = BusquedaLineal(numeros, numeroBuscar);
+            int resultadoBinaria = BusquedaBinaria(numeros, numeroBuscar);
+
+            Console.WriteLine("\n--- RESULTADOS ---");
+
+            if (resultadoLineal != -1)
+            {
+                Console.WriteLine("Busqueda Lineal: Numero encontrado en la posicion " + resultadoLineal);
+            }
+            else
+            {
+                Console.WriteLine("Busqueda Lineal: Numero NO encontrado");
+            }
+
+            if (resultadoBinaria != -1)
+            {
+                Console.WriteLine("Busqueda Binaria: Numero encontrado en la posicion " + resultadoBinaria);
+            }
+            else
+            {
+                Console.WriteLine("Busqueda Binaria: Numero NO encontrado");
+            }
+
+            Console.ReadKey();
+        }
+
+        static int BusquedaLineal(int[] arreglo, int numero)
+        {
+            for (int i = 0; i < arreglo.Length; i++)
+            {
+                if (arreglo[i] == numero)
+                {
+                    return i; 
+                }
+            }
+
+            return -1; 
+        }
+
+        static int BusquedaBinaria(int[] arreglo, int numero)
+        {
+            int inicio = 0;
+            int fin = arreglo.Length - 1;
+
+            while (inicio <= fin)
+            {
+                int mitad = (inicio + fin) / 2;
+
+                if (arreglo[mitad] == numero)
+                {
+                    return mitad;
+                }
+                else if (numero < arreglo[mitad])
+                {
+                    fin = mitad - 1;
+                }
+                else
+                {
+                    inicio = mitad + 1;
+                }
+            }
+
+            return -1; 
+        }
+    }
+}
---
+++ Metodos de busqueda/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// La información general de un ensamblado se controla mediante el siguiente 
+// conjunto de atributos. Cambie estos valores de atributo para modificar la información
+// asociada a un ensamblado.
+[assembly: AssemblyTitle("Metodos de busqueda")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Metodos de busqueda")]
+[assembly: AssemblyCopyright("Copyright ©  2026")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Si establece ComVisible en false, los tipos de este ensamblado no estarán visibles 
+// para los componentes COM.  Si es necesario obtener acceso a un tipo en este ensamblado desde 
+// COM, establezca el atributo ComVisible en true en este tipo.
+[assembly: ComVisible(false)]
+
+// El siguiente GUID sirve como id. de typelib si este proyecto se expone a COM.
+[assembly: Guid("d7b9b092-af6c-4b61-b2f5-143647371937")]
+
+// La información de versión de un ensamblado consta de los cuatro valores siguientes:
+//
+//      Versión principal
+//      Versión secundaria
+//      Número de compilación
+//      Revisión
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
